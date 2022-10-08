6ec9b50ab878df237c8ebd54fe95e637d7327c21
vwkd <33468089+vwkd@users.noreply.github.com>
2022-10-08 13:34:35 +0400

    checkbox and reset.css

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,3 +1,27 @@
+import { Head } from "$fresh/runtime.ts";
+
 export default function Home() {
-  return null;
+  return (
+    <div>
+      <Head>
+        <link rel="stylesheet" href="/reset.css" />
+      </Head>
+      <input
+        type="checkbox"
+        class="w-6 h-6 checked:bg-sky-500 border border-slate-300 rounded-md shadow-md focus:outline-none focus:ring-1 focus:ring-sky-500 items-center justify-center after:content-[''] after:w-3 after:h-3 after:origin-bottom-left after:[clip-path:polygon(14%44%,0%65%,50%100%,100%16%,80%0%,43%62%)] checked:after:bg-white"
+      />
+    </div>
+  );
 }
+
+
+
+
+
+
+
+
+
+
+
+
